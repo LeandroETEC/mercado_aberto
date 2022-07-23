3fd193067d8a5cdc46db5f1deb586072727039f4
leandror <leandro.oliveira372@etec.sp.gov.br>
2022-07-23 12:33:15 -0300

    Separei em outras e comecei a tornalas dinamicas

---
+++ lib/components/cardAssinatura.dart
@@ -0,0 +1,46 @@
+import "package:flutter/material.dart";
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class assinatura extends StatelessWidget {
+  const assinatura({Key? key, required this.titulo}) : super(key: key);
+
+  final String titulo;
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+   return Container(
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                gradient: const LinearGradient(
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                    colors: [Color(0xffaa0f91), Color(0xff171955)]),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.4),
+                    spreadRadius: 0.5,
+                    blurRadius: 2,
+                    offset: const Offset(0, 1.5),
+                  )
+                ],
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children:  [
+                  Text(
+                    titulo,
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  Icon(
+                    Icons.chevron_right,
+                    color: Colors.white,
+                  )
+                ],
+              ),
+            );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/components/cardFreteGratis.dart';
 import 'components/campoPesquisa.dart';
+import 'components/cardAssinatura.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -126,81 +129,11 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ),
             const Padding(padding: EdgeInsets.all(5)),
-            Container(
-              padding: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                gradient: const LinearGradient(
-                    begin: Alignment.centerLeft,
-                    end: Alignment.centerRight,
-                    colors: [Color(0xffaa0f91), Color(0xff171955)]),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.4),
-                    spreadRadius: 0.5,
-                    blurRadius: 2,
-                    offset: const Offset(0, 1.5),
-                  )
-                ],
-                borderRadius: BorderRadius.circular(5),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
-                  Text(
-                    "Assine o nível 6 por R\$ 9,90/mês!",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  Icon(
-                    Icons.chevron_right,
-                    color: Colors.white,
-                  )
-                ],
-              ),
-            ),
+            assinatura(titulo: "assina logo",),
             const Padding(padding: EdgeInsets.all(5)),
-            Container(
-              padding: const EdgeInsets.only(
-                top: 5,
-                bottom: 5,
-              ),
-              decoration: BoxDecoration(
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.4),
-                    spreadRadius: 0.5,
-                    blurRadius: 2,
-                    offset: const Offset(0, 1.5),
-                  )
-                ],
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(5),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Icon(
-                    Icons.local_shipping_outlined,
-                    color: Color(0xff43b176),
-                  ),
-                  Text(
-                    " Frete Grátis",
-                    style: TextStyle(
-                      color: Color(0xff43b176),
-                      fontWeight: FontWeight.bold,
-                      fontSize: 12,
-                    ),
-                  ),
-                  Text(
-                    " em milhões de produtos a partir de RS79",
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 12,
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            freteGratis(),
             const Padding(padding: EdgeInsets.all(10)),
+            assinatura(titulo: "assine o novo cartao noob")
           ],
         ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
