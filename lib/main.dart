09233dce4bb1eb8e80b3279cb739c333e3c6a00b
leandror <leandro.oliveira372@etec.sp.gov.br>
2022-07-23 09:33:15 -0300

    mudei o codigo

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,33 +13,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Mercado Aberto'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -51,71 +31,201 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        leading: IconButton(onPressed: () {}, icon: Icon(Icons.menu)) ,
-        title: TextField( obscureText: false, 
-        decoration: InputDecoration(border: OutlineInputBorder(), labelText: "Bora pesquisar?")), 
-        
-        actions: <Widget>[
-          IconButton(onPressed: () {}, icon: Icon(Icons.done_rounded))
-        ],  backgroundColor:  Colors.yellow
-       , bottom:  PreferredSize(
-        preferredSize: const Size.fromHeight(80.0) /*aumentar esse valor aumenta o tamamnho da barra*/ , child: Text("teste"),),
-      ),
-      body:
-        Container(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topCenter, end: Alignment.bottomCenter,
-                 colors: <Color>[
-                Color(0xfff5d415),
-                Color(0xfff5f5f5),
-              ]
-              )
+        toolbarHeight: 86,
+        elevation: 0,
+        backgroundColor: Color(0xffffdb15),
+        leading: Padding(
+          padding: const EdgeInsets.only(top: 5),
+          child: IconButton(
+            onPressed: () => Scaffold.of(context).openDrawer(),
+            icon: const Icon(
+              Icons.menu,
+              color: Colors.black,
+              size: 32,
             ),
-          child: Container(            
-            child: Row(
-              children: [
-          //tendi +/-, mas o expanded faz com q pegue o espaço disponivel
-                Expanded(child: Text("Linha 1", textAlign: TextAlign.center),) ,
-                Expanded(child: Text("Linha 2",textAlign: TextAlign.center),) ,
-                Expanded(child: Text("Linha 3",textAlign: TextAlign.center),) 
-      ],
-      )
-      
-      
-      
-      ),
+          ),
         ),
-      
-    
-       // This trailing comma makes auto-formatting nicer for build methods.
+        actions: [
+          Container(
+            width: MediaQuery.of(context).size.width - 110,
+            padding: const EdgeInsets.only(top: 10),
+            child: TextField(
+              onSubmitted: (test) => {},
+              decoration: const InputDecoration(
+                isDense: true,
+                contentPadding: EdgeInsets.only(top: 5),
+                filled: true,
+                fillColor: Colors.white,
+                hintText: "Buscar no Mercado Aberto",
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color(0xffffdb15),
+                    width: 0,
+                  ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(30.0),
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color(0xffffdb15),
+                    width: 0,
+                  ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(30.0),
+                  ),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color(0xffffdb15),
+                    width: 0,
+                  ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(30.0),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(padding: EdgeInsets.all(6)),
+          Padding(
+            padding: EdgeInsets.only(top: 10),
+            child: Icon(
+              Icons.shopping_cart_outlined,
+              color: Colors.black,
+              size: 32,
+            ),
+          ),
+          Padding(padding: EdgeInsets.all(6)),
+        ],
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(4.0),
+          child: Container(
+            color: Color(0xffffdb15),
+            height: 40.0,
+            child: Row(
+              children: const [
+                Padding(padding: EdgeInsets.all(5)),
+                Icon(
+                  Icons.pin_drop_outlined,
+                  size: 22,
+                ),
+                Padding(padding: EdgeInsets.all(5)),
+                Text("Enviar para"),
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: Container(
+        padding: const EdgeInsets.only(
+          left: 15,
+          right: 15,
+        ),
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: FractionalOffset(0.5, 0.2),
+            colors: [
+              Color(0xffffdb15),
+              Color(0xfff5f5f5),
+            ],
+          ),
+        ),
+        child: Column(
+          children: [
+            const Padding(padding: EdgeInsets.all(5)),
+            Container(
+              padding: const EdgeInsets.all(3),
+              height: 180,
+              child: Image.network(
+                'https://http2.mlstatic.com/D_NQ_NP650326-MLA50239834698_062022-B.jpg',
+              ),
+            ),
+            const Padding(padding: EdgeInsets.all(5)),
+            Container(
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                gradient: const LinearGradient(
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                    colors: [Color(0xffaa0f91), Color(0xff171955)]),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.4),
+                    spreadRadius: 0.5,
+                    blurRadius: 2,
+                    offset: const Offset(0, 1.5),
+                  )
+                ],
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Text(
+                    "Assine o nível 6 por R\$ 9,90/mês!",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  Icon(
+                    Icons.chevron_right,
+                    color: Colors.white,
+                  )
+                ],
+              ),
+            ),
+            const Padding(padding: EdgeInsets.all(5)),
+            Container(
+              padding: const EdgeInsets.only(
+                top: 5,
+                bottom: 5,
+              ),
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.4),
+                    spreadRadius: 0.5,
+                    blurRadius: 2,
+                    offset: const Offset(0, 1.5),
+                  )
+                ],
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(
+                    Icons.local_shipping_outlined,
+                    color: Color(0xff43b176),
+                  ),
+                  Text(
+                    " Frete Grátis",
+                    style: TextStyle(
+                      color: Color(0xff43b176),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 12,
+                    ),
+                  ),
+                  Text(
+                    " em milhões de produtos a partir de RS79",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 12,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const Padding(padding: EdgeInsets.all(10)),
+          ],
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
